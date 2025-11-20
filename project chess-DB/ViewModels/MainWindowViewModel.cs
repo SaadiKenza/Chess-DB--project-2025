d7cf6e9552b9190d600bef2ed85bec1677f9be1d
Saadi <23268@ecam.be>
2025-11-20 20:23:11 +0100

    Add icons for the navbar

--- project chess-DB/ViewModels/MainWindowViewModel.cs
+++ project chess-DB/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 namespace project_chess_DB.ViewModels;
@@ -23,9 +26,9 @@ public partial class MainWindowViewModel : ViewModelBase
     }
     public ObservableCollection<ListItemTemplate> Items { get; } = new()
     {
-        new ListItemTemplate(typeof(DashboardPageViewModel)),
-        new ListItemTemplate(typeof(PlayerPageViewModel)),
-        new ListItemTemplate(typeof(CompetitionPageViewModel)),
+        new ListItemTemplate(typeof(DashboardPageViewModel),"home_regular"),
+        new ListItemTemplate(typeof(PlayerPageViewModel),"calendar_person_regular"),
+        new ListItemTemplate(typeof(CompetitionPageViewModel),"book_database_regular"),
     };
     [RelayCommand]
     private void TriggerPane()
@@ -37,11 +40,14 @@ public partial class MainWindowViewModel : ViewModelBase
 
 public class ListItemTemplate
 {
-    public ListItemTemplate(Type type)
+    public ListItemTemplate(Type type, string iconKey)
     {
         ModelType = type;
         Label = type.Name.Replace("PageViewModel", "");
+        Application.Current!.TryFindResource(iconKey, out var res);
+        ListItemIcon = (StreamGeometry)res!;
     }
     public string Label { get; }
     public Type ModelType { get; }
+    public StreamGeometry ListItemIcon { get; }
 }
