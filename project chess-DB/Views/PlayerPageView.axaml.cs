3fedb8fbe83f87c384d4c17d24ef9c13195071f2
Saadi <23268@ecam.be>
2025-12-13 21:34:25 +0100

    Add constraints for the form and table

--- project chess-DB/Views/PlayerPageView.axaml.cs
+++ project chess-DB/Views/PlayerPageView.axaml.cs
@@ -1,6 +1,9 @@
 using Avalonia.Controls;
 using Avalonia.Metadata;
 using project_chess_DB.ViewModels;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using System.Linq;
 using project_chess_DB.Models;
 using project_chess_DB.Services;
 
@@ -13,6 +16,35 @@ public partial class PlayerPageView : UserControl
     public PlayerPageView()
     {
         InitializeComponent();
+        var txtAge = this.FindControl<TextBox>("TxtAge");
+        var txtElo = this.FindControl<TextBox>("TxtElo");
+        var txtPhonenumber = this.FindControl<TextBox>("TxtPhonenumber");
+        var txtLastname = this.FindControl<TextBox>("TxtLastname");
+        var txtFirstname = this.FindControl<TextBox>("TxtFirstname");
+        var txtCountry = this.FindControl<TextBox>("TxtCountry");
+        txtAge?.AddHandler(TextInputEvent, Numeric_TextInput, RoutingStrategies.Tunnel);
+        txtElo?.AddHandler(TextInputEvent, Numeric_TextInput, RoutingStrategies.Tunnel);
+        txtPhonenumber?.AddHandler(TextInputEvent, Numeric_TextInput, RoutingStrategies.Tunnel);
+        txtLastname?.AddHandler(TextInputEvent, Letters_TextInput, RoutingStrategies.Tunnel);
+        txtFirstname?.AddHandler(TextInputEvent, Letters_TextInput, RoutingStrategies.Tunnel);
+        txtCountry?.AddHandler(TextInputEvent, Letters_TextInput, RoutingStrategies.Tunnel);
+
+    }
+    private void DataGrid_PreparingCellForEdit(object? sender, DataGridPreparingCellForEditEventArgs e)
+    {
+        if (e.EditingElement is TextBox textBox)
+        {
+            var header = e.Column.Header?.ToString();
+
+            if (header == "Age" || header == "Elo")
+            {
+                textBox.AddHandler(TextInputEvent, Numeric_TextInput, RoutingStrategies.Tunnel);
+            }
+            else if (header == "Last Name" || header == "First Name" || header == "Country")
+            {
+                textBox.AddHandler(TextInputEvent, Letters_TextInput, RoutingStrategies.Tunnel);
+            }
+        }
     }
 
     private void DataGrid_CellEditEnded(object? sender, DataGridCellEditEndedEventArgs e)
@@ -23,5 +55,24 @@ public partial class PlayerPageView : UserControl
             repo.UpdatePlayer(editedPlayer);
         }
 
+    }
+    private void Numeric_TextInput(object? sender, TextInputEventArgs e)
+    {
+        if (!string.IsNullOrEmpty(e.Text) && !e.Text.All(char.IsDigit))
+        {
+            e.Handled = true;
+        }
+    }
+    private void Letters_TextInput(object? sender, TextInputEventArgs e)
+    {
+        if (!string.IsNullOrEmpty(e.Text))
+        {
+            bool isValid = e.Text.All(c => char.IsLetter(c) || c == ' ' || c == '-');
+
+            if (!isValid)
+            {
+                e.Handled = true;
+            }
+        }
     }
 }
